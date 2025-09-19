4bec0ca851371c1fda5ec803f8e38dbe6d49c708
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-09-19 09:37:32 +0200

    Deploy app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2515189096"
+    serviceWorkerVersion: "2854224297"
   }
 });
